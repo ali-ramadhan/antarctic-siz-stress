f6a07ce2bae0059240f49ad0884f9addb97423a8
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2017-10-14 12:37:09 -0400

    Can load and play with MDT dataset.

--- wind_stress.py
+++ wind_stress.py
@@ -1,18 +1,19 @@
 import numpy as np
+import netCDF4
 
 from os import path
-import logging.config, logging
+import logging.config
+import logging
 
-logging_config_path = path.join(path.dirname(path.abspath(__file__)), 'logging.ini')
+cwd = path.dirname(path.abspath(__file__))  # Current Working Directory
+
+logging_config_path = path.join(cwd, 'logging.ini')
 logging.config.fileConfig(logging_config_path)
 logger = logging.getLogger(__name__)
 
-# Physical constants
-# ...
-
 # Data directory string constants
 # Everything under a ./data/ softlink?
-# ...
+data_dir_path = path.join(cwd, 'data/')
 
 
 def distance(ϕ1, λ1, ϕ2, λ2):
@@ -30,8 +31,25 @@ def distance(ϕ1, λ1, ϕ2, λ2):
 
     a = np.sin(Δϕ/2)**2 + np.cos(ϕ1) * np.cos(ϕ2) * np.sin(Δλ/2)**2
     c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a))
-    d = R*c
+    return R*c
 
-    return d
+
+class MDTDataset(object):
+    MDT_filename = 'mdt_cnes_cls2013_global.nc'
+    MDT_file_path = path.join(data_dir_path, MDT_filename)
+
+    def __init__(self):
+        logger.info('MDT class initializing. Loading MDT dataset: %s', self.MDT_filename)
+        self.MDT_dataset = netCDF4.Dataset(self.MDT_file_path)
+
+        logger.info('Successfully loaded MDT dataset: %s', self.MDT_filename)
+        logger.info('Title: %s', self.MDT_dataset.title)
+        logger.info('Data model: %s', self.MDT_dataset.data_model)
+        logger.info('Dimensions: \n %s', self.MDT_dataset.dimensions)
+
 
 if __name__ == '__main__':
+    logger.info("Logger works I guess!")
+    d = distance(24, 25, 26, 27)
+    logger.info("That distance is %f m or %f km.", d, d/1000)
+    MDT_dataset = MDTDataset()
