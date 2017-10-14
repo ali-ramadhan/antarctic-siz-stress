181a7a98ada55aeaf8daf649c8170d87eadd8f0b
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2017-10-14 11:36:14 -0400

    Greek letters and unicode in Python!

--- wind_stress.py
+++ wind_stress.py
@@ -1,12 +1,28 @@
 import numpy as np
 
-def distance(lat1, lon1, lat2, lon2):
-    R = 6371e3  # average radius of the earth [m]
-                # Could be upgraded to be location-dependent using a simple formula
+# Physical constants
+# ...
 
-    dLat = np.deg2rad(lat2 - lat1)
-    dLon = np.deg2rad(lon2 - lon1)
-    a = np.sin(dLat/2)*np.sin(dLat/2) + np.cos(np.deg2rad(lat1))*np.cos(np.deg2rad(lat2))*np.sin(dLon/2)**2
+# Data directory string constants
+# Everything under a ./data/ softlink?
+# ...
+
+
+def distance(ϕ1, λ1, ϕ2, λ2):
+    # Calculate the distance between two points on the Earth (ϕ1, λ1) and (ϕ1, λ1) using the Haversine formula.
+    # See: http://www.movable-type.co.uk/scripts/latlong.html
+    # Latitudes are denoted by ϕ while longitudes are denoted by λ.
+
+    # Earth radius R could be upgraded to be location-dependent using a simple formula.
+    # See: https://en.wikipedia.org/wiki/Earth_radius#Location-dependent_radii
+    R = 6371e3  # average radius of the earth [m]
+
+    # Convert all angles to radians
+    ϕ1, λ1, ϕ2, λ2 = np.deg2rad([ϕ1, λ1, ϕ2, λ2])
+    Δϕ = ϕ2 - ϕ1
+    Δλ = λ2 - λ1
+
+    a = np.sin(Δϕ/2)**2 + np.cos(ϕ1) * np.cos(ϕ2) * np.sin(Δλ/2)**2
     c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a))
     d = R*c
 
