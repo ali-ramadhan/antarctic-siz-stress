e38afad338035cba22ece16da0f0b1732b616461
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2017-10-15 17:40:06 -0400

    Screwing up the lat/lon conversion to x,y...

--- wind_stress.py
+++ wind_stress.py
@@ -83,6 +83,9 @@ class MDTDataset(object):
         # Calculate the x and y derivatives of MDT at the grid point ij using a second-order centered finite difference
         # approximation.
 
+        assert -90 <= lat <= 90, "Latitude value {} out of bounds!".format(lat)
+        assert 0 <= lon <= 360, "Longitude value {} out of bounds!".format(lon)
+
         dLat = self.MDT_dataset.variables['lat'][1] - self.MDT_dataset.variables['lat'][0]
         dLon = self.MDT_dataset.variables['lon'][1] - self.MDT_dataset.variables['lon'][0]
 
@@ -118,11 +121,18 @@ class SeaIceDataset(object):
     sea_ice_dir_path = path.join(data_dir_path, 'NOAA_NSIDC_G02202_V3_SEA_ICE_CONCENTRATION', 'south', 'daily')
 
     def date2filepath(self, date):
-        filename = 'seaice_conc_daily_sh_f17_2011' + str(date.month) + str(date.day) + '_v03r00.nc'
+        filename = 'seaice_conc_daily_sh_f17_' + str(date.year) + str(date.month).zfill(2) + str(date.day)\
+                   + '_v03r00.nc'
         return path.join(self.sea_ice_dir_path, str(date.year), filename)
 
+    def latlon2xy(self, lat, lon):
+        d = distance(lat, lon, -90, 0)
+        x = d * np.cos(np.deg2rad(lon))
+        y = d * np.sin(np.deg2rad(lon-90))
+        return x, y
+
     def __init__(self):
-        test_dataset_date = datetime.date(2011, 12, 31)
+        test_dataset_date = datetime.date(2015, 7, 31)
         test_dataset_filepath = self.date2filepath(test_dataset_date)
         logger.info('Sea ice class initializing. Loading sample sea ice dataset: %s', test_dataset_filepath)
         self.sea_ice_dataset = netCDF4.Dataset(test_dataset_filepath)
@@ -139,12 +149,40 @@ class SeaIceDataset(object):
             dim_string = dim_string + dim_name + '(' + str(dim_size) + ') '
         logger.info('Dimensions: %s', dim_string)
 
+    def sea_ice_concentration(self, lat, lon, day):
+        assert -90 <= lat <= 90, "Latitude value {} out of bounds!".format(lat)
+        assert -180 <= lon <= 180, "Longitude value {} out of bounds!".format(lon)
+
+        max_i = min(self.sea_ice_dataset.variables['latitude'].shape)
+        lat_diag = np.array([self.sea_ice_dataset.variables['latitude'][i][i] for i in range(max_i)])
+
+        lat_idx = np.abs(lat_diag - lat).argmin()
+        lon_idx = np.abs(np.array(self.sea_ice_dataset.variables['longitude'][lat_idx]) - lon).argmin()
+
+        lat_ij = self.sea_ice_dataset.variables['latitude'][lon_idx][lat_idx]
+        lon_ij = self.sea_ice_dataset.variables['longitude'][lat_idx][lon_idx]
+
+        logger.debug("lat = %f, lon = %f", lat, lon)
+        logger.debug("lat_idx = %d, lon_idx = %d", lat_idx, lon_idx)
+        logger.debug("lat_ij = %f, lon_ij = %f", lat_ij, lon_ij)
+
+        x, y = self.latlon2xy(lat, lon)
+        idx_x = np.abs(np.array(self.sea_ice_dataset.variables['xgrid']) - x).argmin()
+        idx_y = np.abs(np.array(self.sea_ice_dataset.variables['ygrid']) - y).argmin()
+        lat_xy = self.sea_ice_dataset.variables['latitude'][idx_x][idx_y]
+        lon_xy = self.sea_ice_dataset.variables['longitude'][idx_x][idx_y]
+
+        logger.debug("x = %f, y = %f", x, y)
+        logger.debug("idx_x = %d, idx_y = %f", idx_x, idx_y)
+        logger.debug("lat_xy = %f, lon_xy = %f", lat_xy, lon_xy)
+
 if __name__ == '__main__':
-    d = distance(24, 25, 26, 27)
-    logger.info("That distance is %f m or %f km.", d, d/1000)
+    dist = distance(24, 25, 26, 27)
+    logger.info("That distance is %f m or %f km.", dist, dist/1000)
 
     MDT = MDTDataset()
     print(MDT.get_MDT(-60, 135+180))
     print(MDT.u_geo_mean(-60, 135+180))
 
     sea_ice = SeaIceDataset()
+    print(sea_ice.sea_ice_concentration(-60, 133, datetime.date(2015, 7, 31)))
