9723f156787098983d884eb694187500d2c8a0f3
Ali Ramadhan <ali.ramadhan@uwaterloo.ca>
2017-11-07 13:20:08 -0500

    Can calculate tau and u_Ekman now.

--- constants.py
+++ constants.py
@@ -16,8 +16,17 @@ Omega = 7.292115e-5  # rotation rate of the Earth [rad/s]
 # See: https://en.wikipedia.org/wiki/Earth_radius#Location-dependent_radii
 R = 6371.228e3  # average radius of the earth [m]
 
-lat_min = -50
-lat_max = 70
+rho_air = 1.225  # [kg/m^3]
+rho_seawater = 1025  # [kg/m^3]
+C_air = 0.00125  # Drag coefficient
+C_seawater = 0.0055  # Draf coefficient
+
+f_0 = 1.46e-4  # Coriolis parameter
+rho_0 = 1027.5  # "reference" density [kg/m^3]
+D_e = 20  # Ekman layer depth [m]
+
+lat_min = -70
+lat_max = -50
 lon_min = -180
 lon_max = 180
 
--- wind_stress.py
+++ wind_stress.py
@@ -10,6 +10,7 @@
 # Longitude = -180 to 180
 
 import datetime
+import numpy as np
 
 # Configure logger first before importing any sub-module that depend on the logger being already configured.
 import logging.config
@@ -24,17 +25,48 @@ if __name__ == '__main__':
     from SeaIceMotionDataReader import SeaIceMotionDataReader
 
     from constants import lat_min, lat_max, lat_step, lon_min, lon_max, lon_step
+    from constants import rho_air, rho_seawater, C_air, C_seawater
+    from constants import f_0, rho_0, D_e
 
-    jan1_2015 = datetime.date(2015, 7, 1)
+    test_date = datetime.date(2015, 7, 1)
 
     mdt = MeanDynamicTopographyDataReader()
-    sea_ice = SeaIceConcentrationDataReader(jan1_2015)
-    wind_vectors = OceanSurfaceWindVectorDataReader(jan1_2015)
-    seaice_motion = SeaIceMotionDataReader(jan1_2015)
+    seaice_conc = SeaIceConcentrationDataReader(test_date)
+    wind_vectors = OceanSurfaceWindVectorDataReader(test_date)
+    seaice_motion = SeaIceMotionDataReader(test_date)
 
     lat = -65
     lon = -175
 
-    print('u_geo_mean = {}'.format(mdt.u_geo_mean(lat, lon)))
-    print('wind = {}'.format(wind_vectors.ocean_surface_wind_vector(lat, lon, jan1_2015)))
-    print('SIM = {}'.format(seaice_motion.seaice_motion_vector(lat, lon, jan1_2015)))
+    n_lat = int((lat_max - lat_min) / lat_step)
+    n_lon = int((lon_max - lon_min) / lon_step)
+
+    lats = np.linspace(lat_min, lat_max, n_lat)
+    lons = np.linspace(lon_min, lon_max, n_lon)
+
+    tau = np.zeros(len(lats), len(lons))
+
+    u_geo_mean = mdt.u_geo_mean(lat, lon)
+    u_wind = wind_vectors.ocean_surface_wind_vector(lat, lon, test_date)
+    alpha = seaice_conc.sea_ice_concentration(lat, lon, test_date)
+    u_ice = seaice_motion.seaice_motion_vector(lat, lon, test_date)
+
+    print('u_geo_mean = {}'.format(u_geo_mean))
+    print('wind = {}'.format(u_wind))
+    print('SIC = {}'.format(alpha))
+    print('SIM = {}'.format(u_ice))
+
+    R_45deg = np.array([[np.cos(np.pi / 4), -np.sin(np.pi / 4)], [np.sin(np.pi / 4), np.cos(np.pi / 4)]])
+
+    tau_error = 1
+    tau = np.array([0, 0])
+    u_Ekman = np.array([0.001, 0.001])
+    while tau_error > 1e-10:
+        tau_air = rho_air * C_air * np.linalg.norm(u_wind) * u_wind
+
+        u_rel = u_ice - (u_geo_mean - u_Ekman)
+        tau_ice = rho_0 * C_seawater * np.linalg.norm(u_rel) * u_rel
+
+        tau_error = np.linalg.norm(tau - (alpha * tau_ice + (1 - alpha) * tau_air))
+        tau = alpha * tau_ice + (1 - alpha) * tau_air
+        u_Ekman = (np.sqrt(2) / (f_0 * rho_0 * D_e)) * np.matmul(R_45deg, tau)
