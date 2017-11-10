c10d2a291add36f94b14a87d09f5f5fe15d4c196
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2017-11-10 13:16:15 -0500

    Interpolating u_geo now.

--- MeanDynamicTopographyDataReader.py
+++ MeanDynamicTopographyDataReader.py
@@ -22,20 +22,31 @@ class MeanDynamicTopographyDataReader(object):
         self.lons = np.array(self.MDT_dataset.variables['lon'])
 
         self.mdt = np.array(self.MDT_dataset.variables['mdt'][0])
-        self.u = np.array(self.MDT_dataset.variables['u'][0])
-        self.v = np.array(self.MDT_dataset.variables['v'][0])
+        self.u_geo = np.array(self.MDT_dataset.variables['u'][0])
+        self.v_geo = np.array(self.MDT_dataset.variables['v'][0])
 
         self.mdt_interp = None
         self.latgrid_interp = None
         self.longrid_interp = None
-        self.interpolate_mdt_dataset()
+        # self.interpolate_mdt_dataset()
+
+        self.ugeo_interp = None
+        self.vgeo_interp = None
+        self.latgrid_interp = None
+        self.longrid_interp = None
+        self.interpolate_u_geo_dataset()
+
+        import matplotlib.pyplot as plt
+        plt.pcolormesh(self.longrid_interp, self.latgrid_interp, self.ugeo_interp)
+        plt.colorbar()
+        plt.show()
 
     def interpolate_mdt_dataset(self):
         from utils import interpolate_dataset
         from constants import data_dir_path
         from constants import lat_min, lat_max, n_lat, lon_min, lon_max, n_lon
 
-        mdt_interp_filename = 'mdt_cnes_cls2013_global' + '_interp_' \
+        mdt_interp_filename = 'mdt_cnes_cls2013_global' + '_interp_mdt_' \
                               + 'lat' + str(lat_min) + '-' + str(lat_max) + '_n' + str(n_lat) + '_' \
                               + 'lon' + str(lon_min) + '-' + str(lon_max) + '_n' + str(n_lon) + '.pickle'
         mdt_interp_filepath = path.join(data_dir_path, 'mdt_cnes_cls2013_global', mdt_interp_filename)
@@ -48,6 +59,32 @@ class MeanDynamicTopographyDataReader(object):
         self.mdt_interp = mdt_interp
         self.latgrid_interp = latgrid_interp
         self.longrid_interp = longrid_interp
+
+    def interpolate_u_geo_dataset(self):
+        from utils import interpolate_dataset
+        from constants import data_dir_path
+        from constants import lat_min, lat_max, n_lat, lon_min, lon_max, n_lon
+
+        interp_filename_suffix = 'lat' + str(lat_min) + '-' + str(lat_max) + '_n' + str(n_lat) + '_' \
+                               + 'lon' + str(lon_min) + '-' + str(lon_max) + '_n' + str(n_lon) + '.pickle'
+
+        ugeo_interp_filename = 'mdt_cnes_cls2013_global' + '_interp_ugeo_' + interp_filename_suffix
+        vgeo_interp_filename = 'mdt_cnes_cls2013_global' + '_interp_vgeo_' + interp_filename_suffix
+        ugeo_interp_filepath = path.join(data_dir_path, 'mdt_cnes_cls2013_global', ugeo_interp_filename)
+        vgeo_interp_filepath = path.join(data_dir_path, 'mdt_cnes_cls2013_global', vgeo_interp_filename)
+
+        mask_value_cond = lambda x: x < -100
+
+        ugeo_interp, latgrid_interp, longrid_interp = \
+            interpolate_dataset(self.u_geo, self.lats, self.lons, ugeo_interp_filepath, mask_value_cond, True)
+        vgeo_interp, latgrid_interp, longrid_interp = \
+            interpolate_dataset(self.v_geo, self.lats, self.lons, vgeo_interp_filepath, mask_value_cond, True)
+
+        self.ugeo_interp = ugeo_interp
+        self.vgeo_interp = vgeo_interp
+        self.latgrid_interp = latgrid_interp
+        self.longrid_interp = longrid_interp
+
 
     def mean_dynamic_topography(self, lat: float, lon: float) -> float:
         if lon < 0:
@@ -85,8 +122,8 @@ class MeanDynamicTopographyDataReader(object):
         logger.debug("idx_lat = {}, idx_lon = {}".format(idx_lat, idx_lon))
         logger.debug("lat[idx_lat] = {}, lon[idx_lon] = {}".format(self.lats[idx_lat], self.lons[idx_lon]))
 
-        u_geo_mean = self.u[idx_lat][idx_lon]
-        v_geo_mean = self.v[idx_lat][idx_lon]
+        u_geo_mean = self.u_geo[idx_lat][idx_lon]
+        v_geo_mean = self.v_geo[idx_lat][idx_lon]
 
         # TODO: Properly check for masked values.
         if u_geo_mean < -100 or v_geo_mean < -100:
