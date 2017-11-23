31225e0b1ed439bc19bfc769ab4322c930f2e226
Ali Ramadhan <ali.ramadhan@uwaterloo.ca>
2017-11-23 15:06:36 -0500

    Renamed variables to make it clear what is a vector, a field, etc.

--- wind_stress.py
+++ wind_stress.py
@@ -45,75 +45,80 @@ if __name__ == '__main__':
 
     lats = np.linspace(lat_min, lat_max, n_lat)
     lons = np.linspace(lon_min, lon_max, n_lon)
+    lons = lons[:-1]  # Remove the +180 longitude as it coincides with the -180 longitude.
 
     logger.info('Calculating surface stress field (tau_x, tau_y) for:')
     logger.info('lat_min = {}, lat_max = {}, lat_step = {}, n_lat = {}'.format(lat_min, lat_max, lat_step, n_lat))
     logger.info('lon_min = {}, lon_max = {}, lon_step = {}, n_lon = {}'.format(lon_min, lon_max, lon_step, n_lon))
 
-    tau_x = np.zeros((len(lats), len(lons)))
-    tau_y = np.zeros((len(lats), len(lons)))
+    # All the fields to save to the netCDF file.
+    tau_x_field = np.zeros((len(lats), len(lons)))
+    tau_y_field = np.zeros((len(lats), len(lons)))
 
     for i in range(len(lats)):
         lat = lats[i]
         f = 2*Omega * np.sin(np.deg2rad(lat))
 
-        progress_percent = 100 * i/len(lats)
-        logger.info('lat = {:.2f}/{:.2f} ({:.1f}%)'.format(lat, lat_max, progress_percent))
-
         for j in range(len(lons)):
             lon = lons[j]
 
-            u_geo_mean = mdt.u_geo_mean(lat, lon, 'interp')
-            u_wind = wind_vectors.ocean_surface_wind_vector(lat, lon, test_date, 'interp')
+            u_geo_mean_vec = mdt.u_geo_mean(lat, lon, 'interp')
+            u_wind_vec = wind_vectors.ocean_surface_wind_vector(lat, lon, test_date, 'interp')
             alpha = seaice_conc.sea_ice_concentration(lat, lon, test_date, 'interp')
-            u_ice = seaice_motion.seaice_motion_vector(lat, lon, test_date, 'interp')
+            u_ice_vec = seaice_motion.seaice_motion_vector(lat, lon, test_date, 'interp')
 
-            if np.isnan(alpha) or np.isnan(u_geo_mean[0]) or np.isnan(u_wind[0]) or np.isnan(u_ice[0]):
-                tau_x[i][j] = np.nan
-                tau_y[i][j] = np.nan
+
+            if np.isnan(alpha) or np.isnan(u_geo_mean_vec[0]) or np.isnan(u_wind_vec[0]) or np.isnan(u_ice_vec[0]):
+                tau_x_field[i][j] = np.nan
+                tau_y_field[i][j] = np.nan
                 continue
 
+            # Use the Modified Richardson iteration to calculate tau and u_Ekman. Here we set the variables to arbitrary
+            # initial guesses.
             iter_count = 0
-            tau_residual = np.array([1, 1])
+            tau_vec_residual = np.array([1, 1])
             tau_relative_error = 1
-            tau = np.array([0, 0])
-            u_Ekman = np.array([0.001, 0.001])
+            tau_vec = np.array([0, 0])
+            u_Ekman_vec = np.array([0.001, 0.001])
             omega = 0.01  # Richardson relaxation parameter
 
-            while np.linalg.norm(tau_residual) > 1e-5:
+            while np.linalg.norm(tau_vec_residual) > 1e-5:
                 iter_count = iter_count + 1
                 if iter_count > 50:
                     logger.warning('iter_acount exceeded 50 during calculation of tau and u_Ekman.')
                     logger.warning('tau = {}, u_Ekman = {}, tau_residual = {}, tau_rel_error = {:.4f}'
-                                   .format(tau, u_Ekman, tau_residual, tau_relative_error))
+                                   .format(tau_vec, u_Ekman_vec, tau_vec_residual, tau_relative_error))
                     break
 
-                if np.linalg.norm(tau) > 10:
+                if np.linalg.norm(tau_vec) > 10:
                     logger.warning('Large tau = {}, u_geo_mean = {}, u_wind = {}, alpha = {:.4f}, u_ice = {}'
-                                   .format(tau, u_geo_mean, u_wind, alpha, u_ice))
+                                   .format(tau_vec, u_geo_mean_vec, u_wind_vec, alpha, u_ice_vec))
                     break
 
-                tau_air = rho_air * C_air * np.linalg.norm(u_wind) * u_wind
+                tau_air = rho_air * C_air * np.linalg.norm(u_wind_vec) * u_wind_vec
 
-                u_Ekman = (np.sqrt(2) / (f * rho_0 * D_e)) * np.matmul(R_45deg, tau)
-                u_rel = u_ice - (u_geo_mean - u_Ekman)
-                tau_ice = rho_0 * C_seawater * np.linalg.norm(u_rel) * u_rel
-                tau = alpha * tau_ice + (1 - alpha) * tau_air
+                u_Ekman_vec = (np.sqrt(2) / (f * rho_0 * D_e)) * np.matmul(R_45deg, tau_vec)
+                u_rel_vec = u_ice_vec - (u_geo_mean_vec - u_Ekman_vec)
+                tau_ice_vec = rho_0 * C_seawater * np.linalg.norm(u_rel_vec) * u_rel_vec
+                tau_vec = alpha * tau_ice_vec + (1 - alpha) * tau_air
 
-                tau_residual = tau - (alpha * tau_ice + (1 - alpha) * tau_air)
-                tau_relative_error = np.linalg.norm(tau_residual)/np.linalg.norm(tau)
+                tau_vec_residual = tau_vec - (alpha * tau_ice_vec + (1 - alpha) * tau_air)
+                tau_relative_error = np.linalg.norm(tau_vec_residual)/np.linalg.norm(tau_vec)
 
-                tau = tau + omega*tau_residual
+                tau_vec = tau_vec + omega*tau_vec_residual
 
-                if np.isnan(tau[0]) or np.isnan(tau[1]):
+                if np.isnan(tau_vec[0]) or np.isnan(tau_vec[1]):
                     logger.warning('NaN tau = {}, u_geo_mean = {}, u_wind = {}, alpha = {:.4f}, u_ice = {}'
-                                   .format(tau, u_geo_mean, u_wind, alpha, u_ice))
+                                   .format(tau_vec, u_geo_mean_vec, u_wind_vec, alpha, u_ice_vec))
 
-            tau_x[i][j] = tau[0]
-            tau_y[i][j] = tau[1]
+            tau_x_field[i][j] = tau_vec[0]
+            tau_y_field[i][j] = tau_vec[1]
+
+        progress_percent = 100 * i/(len(lats)-1)
+        logger.info('lat = {:.2f}/{:.2f} ({:.1f}%)'.format(lat, lat_max, progress_percent))
 
     import matplotlib.pyplot as plt
-    plt.pcolormesh(lons, lats, tau_x)
+    plt.pcolormesh(lons, lats, tau_x_field)
     plt.colorbar()
     plt.show()
 
@@ -141,16 +146,16 @@ if __name__ == '__main__':
     lat_var.units = 'degrees west/east'
     lon_var[:] = lons
 
-    tau_var = tau_dataset.createVariable('tau_x', float, ('lat', 'lon'), zlib=True)
-    tau_var.units = 'N/m^2'
-    tau_var.long_name = 'Zonal surface stress'
-    tau_var.positive = 'up'
-    tau_var[:] = tau_x
+    tau_x_var = tau_dataset.createVariable('tau_x', float, ('lat', 'lon'), zlib=True)
+    tau_x_var.units = 'N/m^2'
+    tau_x_var.long_name = 'Zonal surface stress'
+    tau_x_var.positive = 'up'
+    tau_x_var[:] = tau_x_field
 
-    tau_var = tau_dataset.createVariable('tau_y', float, ('lat', 'lon'), zlib=True)
-    tau_var.units = 'N/m^2'
-    tau_var.long_name = 'Meridional surface stress'
-    tau_var.positive = 'up'
-    tau_var[:] = tau_y
+    tau_y_var = tau_dataset.createVariable('tau_y', float, ('lat', 'lon'), zlib=True)
+    tau_y_var.units = 'N/m^2'
+    tau_y_var.long_name = 'Meridional surface stress'
+    tau_y_var.positive = 'up'
+    tau_y_var[:] = tau_y_field
 
     tau_dataset.close()
