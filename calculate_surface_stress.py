9b081ec4d650279ea022b2d032952ddd8d887a68
Ali Ramadhan <ali.ramadhan@uwaterloo.ca>
2017-11-30 17:05:20 -0500

    Checkpoint.

--- calculate_surface_stress.py
+++ calculate_surface_stress.py
@@ -32,27 +32,27 @@ def process_day(date):
 
 
 if __name__ == '__main__':
-    # from joblib import Parallel, delayed
+    from joblib import Parallel, delayed
 
-    from SeaIceMotionDataReader import SeaIceMotionDataReader
-    sic = SeaIceMotionDataReader(datetime.date(2015, 7, 16))
-    sic.plot_sea_ice_motion_vector_field()
-    exit()
+    # from SeaIceMotionDataReader import SeaIceMotionDataReader
+    # sic = SeaIceMotionDataReader(datetime.date(2015, 7, 16))
+    # sic.plot_sea_ice_motion_vector_field()
+    # exit()
 
-    # date_in_month = datetime.date(2015, 7, 1)
-    # n_days = calendar.monthrange(date_in_month.year, date_in_month.month)[1]
-    #
-    # Parallel(n_jobs=8)(delayed(process_day)(datetime.date(date_in_month.year, date_in_month.month, day))
-    #                    for day in range(1, n_days+1))
+    date_in_month = datetime.date(2015, 7, 1)
+    n_days = calendar.monthrange(date_in_month.year, date_in_month.month)[1]
+
+    Parallel(n_jobs=8)(delayed(process_day)(datetime.date(date_in_month.year, date_in_month.month, day))
+                       for day in range(1, n_days+1))
 
     # for day in range(1, n_days+1):
     #     current_date = datetime.date(date_in_month.year, date_in_month.month, day)
     #     process_day(current_date)
 
-    # from SurfacetressDataWriter import SurfaceStressDataWriter
-    #
-    # surface_stress_dataset = SurfaceStressDataWriter(None)
-    # surface_stress_dataset.compute_monthly_mean_fields(date_in_month, method='partial_data_ok')
+    from SurfacetressDataWriter import SurfaceStressDataWriter
+
+    surface_stress_dataset = SurfaceStressDataWriter(None)
+    surface_stress_dataset.compute_monthly_mean_fields(date_in_month, method='partial_data_ok')
 
     # from utils import latlon_to_polar_stereographic_xy, polar_stereographic_velocity_vector_to_latlon
     # X, Y = np.meshgrid(np.arange(-180, 180, 5), np.arange(-75, -55, 1))
