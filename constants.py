b7511bc8781477f03b541ce8787c008dd8c88a78
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2018-01-08 16:50:53 -0500

    Diagnostic field constants moved to constants.py.

--- constants.py
+++ constants.py
@@ -58,3 +58,137 @@ u_geo_interp_method = 'linear'
 alpha_interp_method = 'linear'
 u_ice_interp_method = 'linear'
 u_wind_interp_method = 'linear'
+
+""" Constants for diagnostic field plots """
+titles = {
+    'u_geo': 'Zonal geostrophic velocity',
+    'v_geo': 'Meridional geostrophic velocity',
+    'u_wind': 'Zonal wind velocity',
+    'v_wind': 'Meridional wind velocity',
+    'u_ice': 'Zonal sea ice motion',
+    'v_ice': 'Meridional sea ice motion',
+    'alpha': 'Sea ice concentration',
+    'tau_air_x': 'Zonal air surface stress',
+    'tau_air_y': 'Meridional air surface stress',
+    'tau_ice_x': 'Zonal sea ice surface stress',
+    'tau_ice_y': 'Meridional sea ice surface stress',
+    'tau_x': 'Zonal surface stress',
+    'tau_y': 'Meridional surface stress',
+    'u_Ekman': 'Zonal Ekman velocity',
+    'v_Ekman': 'Meridional Ekman velocity',
+    'curl_tau': 'Vertical wind stress curl',
+    'tau_SIZ_x': 'Zonal surface stress in the SIZ',
+    'tau_SIZ_y': 'Meridional surface stress in the SIZ',
+    'w_Ekman': 'Ekman pumping'
+}
+
+gs_coords = {
+    'u_geo': (0, 0),
+    'v_geo': (1, 0),
+    'u_wind': (0, 1),
+    'v_wind': (1, 1),
+    'u_ice': (0, 2),
+    'v_ice': (1, 2),
+    'alpha': (0, 3),
+    'tau_air_x': (0, 4),
+    'tau_air_y': (1, 4),
+    'tau_ice_x': (0, 5),
+    'tau_ice_y': (1, 5),
+    'tau_x': (0, 6),
+    'tau_y': (1, 6),
+    'u_Ekman': (0, 7),
+    'v_Ekman': (1, 7),
+    'curl_tau': (0, 8),
+    'tau_SIZ_x': (slice(2, 5), slice(0, 3)),
+    'tau_SIZ_y': (slice(2, 5), slice(3, 6)),
+    'w_Ekman': (slice(2, 5), slice(6, 9))
+}
+
+scale_factor = {
+    'u_geo': 1,
+    'v_geo': 1,
+    'u_wind': 1,
+    'v_wind': 1,
+    'u_ice': 1,
+    'v_ice': 1,
+    'alpha': 1,
+    'tau_air_x': 1,
+    'tau_air_y': 1,
+    'tau_ice_x': 1,
+    'tau_ice_y': 1,
+    'tau_x': 1,
+    'tau_y': 1,
+    'u_Ekman': 1,
+    'v_Ekman': 1,
+    'curl_tau': 1e6,
+    'tau_SIZ_x': 1,
+    'tau_SIZ_y': 1,
+    'w_Ekman': 3600 * 365  # [m/s] -> [m/year]
+}
+
+colorbar_label = {
+    'u_geo': 'm/s',
+    'v_geo': 'm/s',
+    'u_wind': 'm/s',
+    'v_wind': 'm/s',
+    'u_ice': 'm/s',
+    'v_ice': 'm/s',
+    'alpha': '',
+    'tau_air_x': r'N/m$^2$',
+    'tau_air_y': r'N/m$^2$',
+    'tau_ice_x': r'N/m$^2$',
+    'tau_ice_y': r'N/m$^2$',
+    'tau_x': r'N/m$^2$',
+    'tau_y': r'N/m$^2$',
+    'u_Ekman': 'm/s',
+    'v_Ekman': 'm/s',
+    'curl_tau': r'10$^6$ N/m$^3$',
+    'tau_SIZ_x': r'N/m$^2$',
+    'tau_SIZ_y': r'N/m$^2$',
+    'w_Ekman': r'm/year'
+}
+
+# 'seismic' seems like a stronger version of 'RdBu_r', although I like the colors of 'RdBu_r' better.
+cmaps = {
+    'u_geo': 'seismic',
+    'v_geo': 'seismic',
+    'u_wind': 'seismic',
+    'v_wind': 'seismic',
+    'u_ice': 'seismic',
+    'v_ice': 'seismic',
+    'alpha': 'plasma',
+    'tau_air_x': 'seismic',
+    'tau_air_y': 'seismic',
+    'tau_ice_x': 'seismic',
+    'tau_ice_y': 'seismic',
+    'tau_x': 'seismic',
+    'tau_y': 'seismic',
+    'u_Ekman': 'seismic',
+    'v_Ekman': 'seismic',
+    'curl_tau': 'seismic',
+    'tau_SIZ_x': 'seismic',
+    'tau_SIZ_y': 'seismic',
+    'w_Ekman': 'seismic'
+}
+
+cmap_ranges = {
+    'u_geo': (-1, 1),
+    'v_geo': (-1, 1),
+    'u_wind': (-20, 20),
+    'v_wind': (-20, 20),
+    'u_ice': (-0.2, 0.2),
+    'v_ice': (-0.2, 0.2),
+    'alpha': (0, 1),
+    'tau_air_x': (-0.5, 0.5),
+    'tau_air_y': (-0.5, 0.5),
+    'tau_ice_x': (-0.5, 0.5),
+    'tau_ice_y': (-0.5, 0.5),
+    'tau_x': (-0.5, 0.5),
+    'tau_y': (-0.5, 0.5),
+    'u_Ekman': (-0.5, 0.5),
+    'v_Ekman': (-0.5, 0.5),
+    'curl_tau': (-5, 5),
+    'tau_SIZ_x': (-0.5, 0.5),
+    'tau_SIZ_y': (-0.5, 0.5),
+    'w_Ekman': (-50, 50)
+}
