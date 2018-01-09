45e1c86fb2a14c008e1958a88c5d366e0b01aa3c
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2018-01-09 07:54:49 -0500

    Recomputing monthly mean fields using u_ice monthly means.

--- SeaIceConcentrationDataReader.py
+++ SeaIceConcentrationDataReader.py
@@ -126,7 +126,7 @@ class SeaIceConcentrationDataReader(object):
         if alpha > 1:
             return np.nan
 
-        if alpha > 0 and alpha < 0.15:
+        if 0 < alpha < 0.15:
             return 0
 
         return alpha
