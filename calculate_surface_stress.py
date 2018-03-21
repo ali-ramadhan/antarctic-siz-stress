b4239481e4e5c584ae3239c9376ffb99efa89bb6
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2018-03-20 20:31:55 -0400

    Looking at ice edge movement as well.

--- calculate_surface_stress.py
+++ calculate_surface_stress.py
@@ -709,23 +709,24 @@ def analyze_zero_zonal_stress_line(custom_str=None):
     import cartopy.crs as ccrs
     from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
 
-    from utils import get_zero_zonal_stress_line, get_netCDF_filepath
-    from constants import output_dir_path
+    from utils import get_netCDF_filepath, get_northward_zero_zonal_stress_line, get_northward_ice_edge
+    from constants import output_dir_path, lon_min, lon_max, n_lon
 
     surface_stress_dir = os.path.join(output_dir_path, 'surface_stress')
 
     years = np.arange(2000, 2016, 1)
 
-    lon_bins = np.arange(-180, 180, 1)
+    lon_bins = np.linspace(lon_min, lon_max, n_lon)
     lat_northward = np.empty((len(lon_bins), len(years)))
     lat_northward[:] = -180
+    ice_lat_northward = np.empty((len(lon_bins), len(years)))
+    ice_lat_northward[:] = -180
 
     land_50m = cartopy.feature.NaturalEarthFeature('physical', 'land', '10m', edgecolor='face',
                                                    facecolor='dimgray', linewidth=0)
     vector_crs = ccrs.PlateCarree()
     fig = plt.figure(figsize=(16, 9))
 
-    # ax = fig.add_subplot(111)
     ax = fig.add_subplot(111, projection=ccrs.SouthPolarStereo())
     ax.add_feature(land_50m)
     ax.set_extent([-110, -50, -75, -65], ccrs.PlateCarree())
@@ -741,35 +742,32 @@ def analyze_zero_zonal_stress_line(custom_str=None):
         tau_filepath = get_netCDF_filepath(date=datetime.date(year, 1, 1), field_type='annual')
         # tau_filepath = get_netCDF_filepath(field_type='seasonal', date=datetime.date(year, 1, 1), season_str='JFM')
 
-        lons, lats = get_zero_zonal_stress_line(tau_filepath)
+        # lons, lats = get_zero_zonal_stress_line(tau_filepath)
+        lon_bins, lat_northward_i = get_northward_zero_zonal_stress_line(tau_filepath)
+        lat_northward[:, i] = lat_northward_i
 
-        for j in range(len(lons)):
-            lon = lons[j]
-            lat = lats[j]
+        bellinghausen = np.logical_and(lon_bins > -150, lon_bins < -30)
 
-            closest_lon_idx = np.abs(lon_bins - lon).argmin()
+        logger.info('({:d}) lat_max={:f}'.format(year, np.nanmax(lat_northward[:, i])))
 
-            if lat > lat_northward[closest_lon_idx][i]:
-                lat_northward[closest_lon_idx][i] = lat
+        # ice_lon_bins, ice_lat_northward_i = get_northward_ice_edge(tau_filepath)
+        # ice_lat_northward[:, i] = ice_lat_northward_i
 
-        logger.info('{:d} max lat: {:f}'.format(year, np.max(lat_northward[:, i])))
+        # plt.plot(lon_bins, lat_northward_i, label=str(year), color=cm(1. * i/NUM_COLORS))
+        # plt.plot(ice_lon_bins, ice_lat_northward_i, color=cm(1. * i/NUM_COLORS))
 
-        # Mask values that we don't have data for.
-        lat_northward[lat_northward[:, i] == -180, i] = np.nan
+        ax.plot(lon_bins[bellinghausen], lat_northward_i[bellinghausen], color=cm(1. * i/NUM_COLORS), linewidth=1, label=str(year), transform=vector_crs)
+        # p = ax.plot(ice_lon_bins, ice_lat_northward_i, linewidth=1, label=str(year), transform=vector_crs)
+        patches = patches + [mpatches.Patch(color=cm(1. * i/NUM_COLORS), label=str(year))]
 
-        # plt.plot(lon_bins, lat_northward[:, i], label=str(year))
-        # ax.scatter(lons, lats, marker=',', s=2, label=str(year))
-
-        p = ax.plot(lon_bins, lat_northward, linewidth=1, label=str(year), transform=vector_crs)
-        patches = patches + [mpatches.Patch(color=p[i].get_color(), label=str(year))]
-
-    ax.gridlines()
+    ax.grid()
 
     # ax.legend()
-    plt.legend(handles=patches, loc='upper right', bbox_to_anchor=(1.03, 1, 1.08, 0.7), ncol=1, mode='expand',
+    plt.legend(handles=patches, loc='upper right', bbox_to_anchor=(1.03, 1), ncol=1, mode='expand', frameon=False,
                borderaxespad=0)
     plt.savefig('tau_x_zero_line' + custom_str + '.png', dpi=600, format='png', transparent=False, bbox_inches='tight')
     plt.show()
+    plt.close()
 
     from scipy import stats
 
@@ -798,6 +796,7 @@ def analyze_zero_zonal_stress_line(custom_str=None):
     # plt.plot(lon_bins, std_errs, label='std_errs')
     plt.legend()
     plt.show()
+    plt.close()
 
 
 if __name__ == '__main__':
