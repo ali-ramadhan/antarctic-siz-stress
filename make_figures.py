301755eeccd35e152738229d636f202f4f89a471
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2018-04-19 16:56:55 -0400

    5 box climo plot now for all vars.

--- make_figures.py
+++ make_figures.py
@@ -19,15 +19,15 @@ logging.config.fileConfig('logging.ini')
 logger = logging.getLogger(__name__)
 
 
-def make_five_box_climo_fig():
+def make_five_box_climo_fig(var):
     from utils import get_netCDF_filepath, get_field_from_netcdf
     from constants import figure_dir_path
 
-    climo_filepath = get_netCDF_filepath(field_type='climo', year_start=1992, year_end=2015)
-    JFM_filepath = get_netCDF_filepath(field_type='seasonal_climo', season_str='JFM', year_start=1992, year_end=2015)
-    AMJ_filepath = get_netCDF_filepath(field_type='seasonal_climo', season_str='AMJ', year_start=1992, year_end=2015)
-    JAS_filepath = get_netCDF_filepath(field_type='seasonal_climo', season_str='JAS', year_start=1992, year_end=2015)
-    OND_filepath = get_netCDF_filepath(field_type='seasonal_climo', season_str='OND', year_start=1992, year_end=2015)
+    climo_filepath = get_netCDF_filepath(field_type='climo', year_start=2005, year_end=2015)
+    JFM_filepath = get_netCDF_filepath(field_type='seasonal_climo', season_str='JFM', year_start=2005, year_end=2015)
+    AMJ_filepath = get_netCDF_filepath(field_type='seasonal_climo', season_str='AMJ', year_start=2005, year_end=2015)
+    JAS_filepath = get_netCDF_filepath(field_type='seasonal_climo', season_str='JAS', year_start=2005, year_end=2015)
+    OND_filepath = get_netCDF_filepath(field_type='seasonal_climo', season_str='OND', year_start=2005, year_end=2015)
 
     lons, lats, climo_tau_x_field = get_field_from_netcdf(climo_filepath, 'tau_x')
 
@@ -47,6 +47,14 @@ def make_five_box_climo_fig():
         'OND': get_field_from_netcdf(OND_filepath, 'tau_y')[2]
     }
 
+    var_fields = {
+        'climo': get_field_from_netcdf(climo_filepath, var)[2],
+        'JFM': get_field_from_netcdf(JFM_filepath, var)[2],
+        'AMJ': get_field_from_netcdf(AMJ_filepath, var)[2],
+        'JAS': get_field_from_netcdf(JAS_filepath, var)[2],
+        'OND': get_field_from_netcdf(OND_filepath, var)[2]
+    }
+
     alpha_fields = {
         'climo': get_field_from_netcdf(climo_filepath, 'alpha')[2],
         'JFM': get_field_from_netcdf(JFM_filepath, 'alpha')[2],
@@ -55,20 +63,12 @@ def make_five_box_climo_fig():
         'OND': get_field_from_netcdf(OND_filepath, 'alpha')[2]
     }
 
-    u_wind_fields = {
-        'climo': get_field_from_netcdf(climo_filepath, 'wind_u')[2],
-        'JFM': get_field_from_netcdf(JFM_filepath, 'wind_u')[2],
-        'AMJ': get_field_from_netcdf(AMJ_filepath, 'wind_u')[2],
-        'JAS': get_field_from_netcdf(JAS_filepath, 'wind_u')[2],
-        'OND': get_field_from_netcdf(OND_filepath, 'wind_u')[2]
-    }
-
     titles = {
-        'climo': 'tau_y_climo',
-        'JFM': 'tau_y_JFM',
-        'AMJ': 'tau_y_AMJ',
-        'JAS': 'tau_y_JAS',
-        'OND': 'tau_y_OND'
+        'climo': var + '_climo',
+        'JFM': var + '_JFM',
+        'AMJ': var + '_AMJ',
+        'JAS': var + '_JAS',
+        'OND': var + '_OND'
     }
 
     gs_coords = {
@@ -89,37 +89,37 @@ def make_five_box_climo_fig():
     gs = GridSpec(2, 4)
     matplotlib.rcParams.update({'font.size': 10})
 
-    for var in tau_x_fields.keys():
-        ax = plt.subplot(gs[gs_coords[var]], projection=ccrs.SouthPolarStereo())
+    for season in tau_x_fields.keys():
+        ax = plt.subplot(gs[gs_coords[season]], projection=ccrs.SouthPolarStereo())
         ax.add_feature(land_50m)
         ax.set_extent([-180, 180, -90, -50], ccrs.PlateCarree())
-        ax.set_title(titles[var])
+        ax.set_title(titles[season])
 
-        im = ax.pcolormesh(lons, lats, tau_y_fields[var], transform=vector_crs, cmap=cm.get_cmap('seismic', 15),
-                           vmin=-0.15, vmax=0.15)
+        # im = ax.contourf(lons, lats, var_fields[season], 14, transform=vector_crs, cmap='seismic',
+        #                  vmin=-0.15, vmax=0.15)
+        im = ax.pcolormesh(lons, lats, var_fields[season], transform=vector_crs, cmap=cm.get_cmap('seismic', 15),
+                           vmin=-0.1, vmax=0.1)
 
-        ax.quiver(lons[::10], lats[::10], figure_fields[var][::10, ::10], tau_y_fields[var][::10, ::10],
-                  transform=vector_crs, units='width', width=0.002, scale=4)
+        if var == 'tau_x' or var == 'tau_y':
+            ax.quiver(lons[::10], lats[::10], tau_x_fields[season][::10, ::10], tau_y_fields[season][::10, ::10],
+                      transform=vector_crs, units='width', width=0.002, scale=4)
 
-        ax.contour(lons, lats, np.ma.array(figure_fields[var], mask=np.isnan(alpha_fields[var])), levels=[0],
+        ax.contour(lons, lats, np.ma.array(tau_x_fields[season], mask=np.isnan(alpha_fields[season])), levels=[0],
                    colors='green', linewidths=2, transform=vector_crs)
-        ax.contour(lons, lats, np.ma.array(u_wind_fields[var], mask=np.isnan(alpha_fields[var])), levels=[0],
-                   colors='gold', linewidths=2, transform=vector_crs)
-        ax.contour(lons, lats, np.ma.array(alpha_fields[var], mask=np.isnan(alpha_fields[var])), levels=[0.15],
+        ax.contour(lons, lats, np.ma.array(alpha_fields[season], mask=np.isnan(alpha_fields[season])), levels=[0.15],
                    colors='black', linewidths=2, transform=vector_crs)
 
-        if var == 'climo':
+        if season == 'climo':
             clb = fig.colorbar(im, ax=ax, extend='both', fraction=0.046, pad=0.04)
-            clb.ax.set_title(r'N/m$^2$')
+            clb.ax.set_title(r'm/s')
 
             zero_stress_line_patch = mpatches.Patch(color='green', label='zero zonal stress line')
-            zero_wind_line_patch = mpatches.Patch(color='gold', label='zero zonal wind line')
             ice_edge_patch = mpatches.Patch(color='black', label='15% ice edge')
 
-            plt.legend(handles=[zero_stress_line_patch, zero_wind_line_patch, ice_edge_patch], loc='lower center',
+            plt.legend(handles=[zero_stress_line_patch, ice_edge_patch], loc='lower center',
                        bbox_to_anchor=(0, -0.05, 1, -0.05), ncol=3, mode='expand', borderaxespad=0)
 
-    png_filepath = os.path.join(figure_dir_path, 'tau_y_seasonal_climo_figure.png')
+    png_filepath = os.path.join(figure_dir_path, var + '_seasonal_climo_figure.png')
 
     tau_dir = os.path.dirname(png_filepath)
     if not os.path.exists(tau_dir):
@@ -137,7 +137,7 @@ def make_melt_rate_diagnostic_fig():
 
     salinity_dataset = SalinityDataset(time_span='A5B2', avg_period='00', grid_size='04', field_type='an')
 
-    climo_filepath = get_netCDF_filepath(field_type='climo', year_start=2005, year_end=2012)
+    climo_filepath = get_netCDF_filepath(field_type='climo', year_start=2005, year_end=2015)
     lons, lats, tau_x = get_field_from_netcdf(climo_filepath, 'tau_x')
     tau_y = get_field_from_netcdf(climo_filepath, 'tau_y')[2]
     alpha = get_field_from_netcdf(climo_filepath, 'alpha')[2]
@@ -154,11 +154,11 @@ def make_melt_rate_diagnostic_fig():
         for j in range(len(lons)):
             salinity[i][j] = salinity_dataset.salinity(lats[i], lons[j], 0)
 
-    logger.info('Smoothing salinity field using Gaussian filter...')
-    from scipy.ndimage import gaussian_filter
-    salinity_smoothed = gaussian_filter(salinity, sigma=3)
-
-    salinity[:] = salinity_smoothed[:]
+    # logger.info('Smoothing salinity field using Gaussian filter...')
+    # from scipy.ndimage import gaussian_filter
+    # salinity_smoothed = gaussian_filter(salinity, sigma=3)
+    #
+    # salinity[:] = salinity_smoothed[:]
 
     for i in range(1, len(lats)-1):
         lat = lats[i]
@@ -298,7 +298,7 @@ def make_melt_rate_diagnostic_fig():
         ax.contour(lons, lats, np.ma.array(alpha, mask=np.isnan(alpha)), levels=[0.15],
                    colors='black', linewidths=2, transform=vector_crs)
 
-    png_filepath = os.path.join(figure_dir_path, 'melt_rate_diagnostic_figure.png')
+    png_filepath = os.path.join(figure_dir_path, 'melt_rate_diagnostic_figure2.png')
 
     tau_dir = os.path.dirname(png_filepath)
     if not os.path.exists(tau_dir):
