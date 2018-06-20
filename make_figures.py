e8d2c6efab4ca20332ab2b97a1fb871e0c05dc1c
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2018-06-20 09:49:13 -0400

    Completely refactored make_zonal_and_contour_averaged_plots.

--- make_figures.py
+++ make_figures.py
@@ -324,15 +324,324 @@ def make_zonal_and_contour_averaged_plots():
 
     from utils import get_netCDF_filepath, get_field_from_netcdf
     from utils import get_northward_zero_zonal_stress_line, get_northward_ice_edge, get_coast_coordinates
-    from constants import D_e
+    from constants import figure_dir_path, D_e, Omega
 
-    tau_filepath = get_netCDF_filepath(field_type='monthly_climo', date=datetime.date(2000, 11, 1), year_start=2005, year_end=2015)
+    tau_filepath = get_netCDF_filepath(field_type='climo', year_start=2005, year_end=2015)
+
     lons, lats, ice_div_field = get_field_from_netcdf(tau_filepath, 'ice_flux_div')
     melt_rate_field = get_field_from_netcdf(tau_filepath, 'melt_rate')[2]
     psi_delta_field = get_field_from_netcdf(tau_filepath, 'psi_delta')[2]
     salinity_field = get_field_from_netcdf(tau_filepath, 'salinity')[2]
     temperature_field = get_field_from_netcdf(tau_filepath, 'temperature')[2]
 
+    climo_tau_x_field = get_field_from_netcdf(tau_filepath, 'tau_x')[2]
+    climo_alpha_field = get_field_from_netcdf(tau_filepath, 'alpha')[2]
+
+    U_Ekman_field = D_e * get_field_from_netcdf(tau_filepath, 'Ekman_u')[2]
+    V_Ekman_field = D_e * get_field_from_netcdf(tau_filepath, 'Ekman_v')[2]
+    dSdx_field = get_field_from_netcdf(tau_filepath, 'dSdx')[2]
+    dSdy_field = get_field_from_netcdf(tau_filepath, 'dSdy')[2]
+
+    melt_rate_field_v2 = np.zeros(melt_rate_field.shape)
+
+    for i in range(1, len(lats) - 1):
+        lat = lats[i]
+        f = 2 * Omega * np.sin(np.deg2rad(lat))  # Coriolis parameter [s^-1]
+
+        progress_percent = 100 * i / (len(lats) - 2)
+        logger.info('(M-F) lat = {:.2f}/-40 ({:.1f}%)'.format(lat, progress_percent))
+        for j in range(len(lons)):
+            U_Ekman = U_Ekman_field[i][j]
+            V_Ekman = V_Ekman_field[i][j]
+            S = salinity_field[i][j]
+            dSdx = dSdx_field[i][j]
+            dSdy = dSdy_field[i][j]
+
+            if not np.isnan(U_Ekman) and not np.isnan(V_Ekman) and not np.isnan(dSdx) and not np.isnan(dSdy):
+                melt_rate_field_v2[i][j] = (U_Ekman * dSdx + V_Ekman * dSdy) / S
+
+    """ Plot some fields """
+    # Add land to the plot with a 1:50,000,000 scale. Line width is set to 0 so that the edges aren't poofed up in
+    # the smaller plots.
+    land_50m = cartopy.feature.NaturalEarthFeature('physical', 'land', '50m', edgecolor='face', facecolor='dimgray',
+                                                   linewidth=0)
+    ice_50m = cartopy.feature.NaturalEarthFeature('physical', 'antarctic_ice_shelves_polys', '50m', edgecolor='face',
+                                                  facecolor='darkgray', linewidth=0)
+    vector_crs = ccrs.PlateCarree()
+
+    # Compute a circle in axes coordinates, which we can use as a boundary for the map. We can pan/zoom as much as we
+    # like - the boundary will be permanently circular.
+    import matplotlib.path as mpath
+    theta = np.linspace(0, 2*np.pi, 100)
+    center, radius = [0.5, 0.5], 0.5
+    verts = np.vstack([np.sin(theta), np.cos(theta)]).T
+    circle = mpath.Path(verts * radius + center)
+
+    fig = plt.figure(figsize=(16, 9))
+    gs = GridSpec(1, 2)
+    matplotlib.rcParams.update({'font.size': 10})
+
+    crs_sps = ccrs.SouthPolarStereo()
+    crs_sps._threshold = 1000.0  # This solves https://github.com/SciTools/cartopy/issues/363
+
+    ax1 = plt.subplot(121, projection=crs_sps)
+
+    ax1.add_feature(land_50m)
+    ax1.add_feature(ice_50m)
+    ax1.set_extent([-180, 180, -90, -50], ccrs.PlateCarree())
+    ax1.set_boundary(circle, transform=ax1.transAxes)
+
+    gl1 = ax1.gridlines(crs=ccrs.PlateCarree(), draw_labels=False, linewidth=1, color='black', alpha=.8, linestyle='--')
+    gl1.xlocator = mticker.FixedLocator([-180, -135, -90, -45, 0, 45, 90, 135, 180])
+    gl1.ylocator = mticker.FixedLocator([-80, -70, -60, -50])
+
+    im1 = ax1.pcolormesh(lons, lats, 1e6 * dSdx_field, transform=vector_crs, cmap=cmocean.cm.balance,
+                         vmin=-1, vmax=1)
+
+    ax1.contour(lons, lats, np.ma.array(climo_alpha_field, mask=np.isnan(climo_alpha_field)),
+                levels=[0.15], colors='black', linewidths=2, transform=vector_crs)
+    ax1.contour(lons, lats, np.ma.array(climo_tau_x_field, mask=np.isnan(climo_alpha_field)), levels=[0],
+                colors='green', linewidths=2, transform=vector_crs)
+
+    ax1.text(0.50, 1.05, r'dS/dx', fontsize=14, va='bottom', ha='center', rotation='horizontal',
+             rotation_mode='anchor', transform=ax1.transAxes)
+
+    ax1.text(0.49, 1.01, '0°', transform=ax1.transAxes)
+    ax1.text(1.0105, 0.49, '90°E', transform=ax1.transAxes)
+    ax1.text(0.47, -0.03, '180°', transform=ax1.transAxes)
+    ax1.text(-0.09, 0.49, '90°W', transform=ax1.transAxes)
+    ax1.text(0.855, 0.895, '45°E', rotation=45, transform=ax1.transAxes)
+    ax1.text(0.85, 0.125, '135°E', rotation=-45, transform=ax1.transAxes)
+    ax1.text(0.07, 0.90, '45°W', rotation=-45, transform=ax1.transAxes)
+    ax1.text(0.06, 0.13, '135°W', rotation=45, transform=ax1.transAxes)
+
+    clb = fig.colorbar(im1, ax=ax1, extend='both', fraction=0.046, pad=0.1)
+    clb.ax.set_title(r'psu/m ($\times 10^6$)')
+
+    ax2 = plt.subplot(122, projection=crs_sps)
+
+    ax2.add_feature(land_50m)
+    ax2.add_feature(ice_50m)
+    ax2.set_extent([-180, 180, -90, -50], ccrs.PlateCarree())
+    ax2.set_boundary(circle, transform=ax2.transAxes)
+
+    gl2 = ax2.gridlines(crs=ccrs.PlateCarree(), draw_labels=False, linewidth=1, color='black', alpha=.8, linestyle='--')
+    gl2.xlocator = mticker.FixedLocator([-180, -135, -90, -45, 0, 45, 90, 135, 180])
+    gl2.ylocator = mticker.FixedLocator([-80, -70, -60, -50])
+
+    im2 = ax2.pcolormesh(lons, lats, 1e6 * dSdy_field, transform=vector_crs, cmap=cmocean.cm.balance,
+                         vmin=-1, vmax=1)
+
+    ax2.contour(lons, lats, np.ma.array(climo_alpha_field, mask=np.isnan(climo_alpha_field)),
+                levels=[0.15], colors='black', linewidths=2, transform=vector_crs)
+    ax2.contour(lons, lats, np.ma.array(climo_tau_x_field, mask=np.isnan(climo_alpha_field)), levels=[0],
+                colors='green', linewidths=2, transform=vector_crs)
+
+    ax2.text(0.50, 1.05, r'dS/dy', fontsize=14, va='bottom', ha='center', rotation='horizontal',
+             rotation_mode='anchor', transform=ax2.transAxes)
+
+    ax2.text(0.49, 1.01, '0°', transform=ax2.transAxes)
+    ax2.text(1.0105, 0.49, '90°E', transform=ax2.transAxes)
+    ax2.text(0.47, -0.03, '180°', transform=ax2.transAxes)
+    ax2.text(-0.09, 0.49, '90°W', transform=ax2.transAxes)
+    ax2.text(0.855, 0.895, '45°E', rotation=45, transform=ax2.transAxes)
+    ax2.text(0.85, 0.125, '135°E', rotation=-45, transform=ax2.transAxes)
+    ax2.text(0.07, 0.90, '45°W', rotation=-45, transform=ax2.transAxes)
+    ax2.text(0.06, 0.13, '135°W', rotation=45, transform=ax2.transAxes)
+
+    clb = fig.colorbar(im2, ax=ax2, extend='both', fraction=0.046, pad=0.1)
+    clb.ax.set_title(r'psu/m ($\times 10^6$)')
+
+    png_filepath = os.path.join(figure_dir_path, 'salinity_derivatives.png')
+
+    tau_dir = os.path.dirname(png_filepath)
+    if not os.path.exists(tau_dir):
+        logger.info('Creating directory: {:s}'.format(tau_dir))
+        os.makedirs(tau_dir)
+
+    logger.info('Saving diagnostic figure: {:s}'.format(png_filepath))
+    plt.savefig(png_filepath, dpi=300, format='png', transparent=False, bbox_inches='tight')
+    plt.close()
+
+    ###
+
+    fig = plt.figure(figsize=(16, 9))
+    gs = GridSpec(1, 2)
+    matplotlib.rcParams.update({'font.size': 10})
+
+    crs_sps = ccrs.SouthPolarStereo()
+    crs_sps._threshold = 1000.0  # This solves https://github.com/SciTools/cartopy/issues/363
+
+    ax1 = plt.subplot(121, projection=crs_sps)
+
+    ax1.add_feature(land_50m)
+    ax1.add_feature(ice_50m)
+    ax1.set_extent([-180, 180, -90, -50], ccrs.PlateCarree())
+    ax1.set_boundary(circle, transform=ax1.transAxes)
+
+    gl1 = ax1.gridlines(crs=ccrs.PlateCarree(), draw_labels=False, linewidth=1, color='black', alpha=.8, linestyle='--')
+    gl1.xlocator = mticker.FixedLocator([-180, -135, -90, -45, 0, 45, 90, 135, 180])
+    gl1.ylocator = mticker.FixedLocator([-80, -70, -60, -50])
+
+    im1 = ax1.pcolormesh(lons, lats, 3600*24*365*np.divide(np.multiply(U_Ekman_field, dSdx_field), salinity_field),
+                         transform=vector_crs, cmap=cmocean.cm.balance, vmin=-1, vmax=1)
+
+    ax1.contour(lons, lats, np.ma.array(climo_alpha_field, mask=np.isnan(climo_alpha_field)),
+                levels=[0.15], colors='black', linewidths=2, transform=vector_crs)
+    ax1.contour(lons, lats, np.ma.array(climo_tau_x_field, mask=np.isnan(climo_alpha_field)), levels=[0],
+                colors='green', linewidths=2, transform=vector_crs)
+
+    ax1.text(0.50, 1.05, r'$\mathcal{U}_{Ek}$ * 1/S * dS/dx', fontsize=14, va='bottom', ha='center', rotation='horizontal',
+             rotation_mode='anchor', transform=ax1.transAxes)
+
+    ax1.text(0.49, 1.01, '0°', transform=ax1.transAxes)
+    ax1.text(1.0105, 0.49, '90°E', transform=ax1.transAxes)
+    ax1.text(0.47, -0.03, '180°', transform=ax1.transAxes)
+    ax1.text(-0.09, 0.49, '90°W', transform=ax1.transAxes)
+    ax1.text(0.855, 0.895, '45°E', rotation=45, transform=ax1.transAxes)
+    ax1.text(0.85, 0.125, '135°E', rotation=-45, transform=ax1.transAxes)
+    ax1.text(0.07, 0.90, '45°W', rotation=-45, transform=ax1.transAxes)
+    ax1.text(0.06, 0.13, '135°W', rotation=45, transform=ax1.transAxes)
+
+    clb = fig.colorbar(im1, ax=ax1, extend='both', fraction=0.046, pad=0.1)
+    clb.ax.set_title(r'm/year')
+
+    ax2 = plt.subplot(122, projection=crs_sps)
+
+    ax2.add_feature(land_50m)
+    ax2.add_feature(ice_50m)
+    ax2.set_extent([-180, 180, -90, -50], ccrs.PlateCarree())
+    ax2.set_boundary(circle, transform=ax2.transAxes)
+
+    gl2 = ax2.gridlines(crs=ccrs.PlateCarree(), draw_labels=False, linewidth=1, color='black', alpha=.8, linestyle='--')
+    gl2.xlocator = mticker.FixedLocator([-180, -135, -90, -45, 0, 45, 90, 135, 180])
+    gl2.ylocator = mticker.FixedLocator([-80, -70, -60, -50])
+
+    im2 = ax2.pcolormesh(lons, lats, 3600*24*365*np.divide(np.multiply(V_Ekman_field, dSdy_field), salinity_field),
+                         transform=vector_crs, cmap=cmocean.cm.balance, vmin=-1, vmax=1)
+
+    ax2.contour(lons, lats, np.ma.array(climo_alpha_field, mask=np.isnan(climo_alpha_field)),
+                levels=[0.15], colors='black', linewidths=2, transform=vector_crs)
+    ax2.contour(lons, lats, np.ma.array(climo_tau_x_field, mask=np.isnan(climo_alpha_field)), levels=[0],
+                colors='green', linewidths=2, transform=vector_crs)
+
+    ax2.text(0.50, 1.05, r'$\mathcal{V}_{Ek}$ * 1/S * dS/dy', fontsize=14, va='bottom', ha='center', rotation='horizontal',
+             rotation_mode='anchor', transform=ax2.transAxes)
+
+    ax2.text(0.49, 1.01, '0°', transform=ax2.transAxes)
+    ax2.text(1.0105, 0.49, '90°E', transform=ax2.transAxes)
+    ax2.text(0.47, -0.03, '180°', transform=ax2.transAxes)
+    ax2.text(-0.09, 0.49, '90°W', transform=ax2.transAxes)
+    ax2.text(0.855, 0.895, '45°E', rotation=45, transform=ax2.transAxes)
+    ax2.text(0.85, 0.125, '135°E', rotation=-45, transform=ax2.transAxes)
+    ax2.text(0.07, 0.90, '45°W', rotation=-45, transform=ax2.transAxes)
+    ax2.text(0.06, 0.13, '135°W', rotation=45, transform=ax2.transAxes)
+
+    clb = fig.colorbar(im2, ax=ax2, extend='both', fraction=0.046, pad=0.1)
+    clb.ax.set_title(r'm/year')
+
+    png_filepath = os.path.join(figure_dir_path, 'U_Ekman_times_salinity_derivatives.png')
+
+    tau_dir = os.path.dirname(png_filepath)
+    if not os.path.exists(tau_dir):
+        logger.info('Creating directory: {:s}'.format(tau_dir))
+        os.makedirs(tau_dir)
+
+    logger.info('Saving diagnostic figure: {:s}'.format(png_filepath))
+    plt.savefig(png_filepath, dpi=300, format='png', transparent=False, bbox_inches='tight')
+    plt.close()
+
+    ###
+
+    fig = plt.figure(figsize=(16, 9))
+    gs = GridSpec(1, 2)
+    matplotlib.rcParams.update({'font.size': 10})
+
+    crs_sps = ccrs.SouthPolarStereo()
+    crs_sps._threshold = 1000.0  # This solves https://github.com/SciTools/cartopy/issues/363
+
+    ax1 = plt.subplot(121, projection=crs_sps)
+
+    ax1.add_feature(land_50m)
+    ax1.add_feature(ice_50m)
+    ax1.set_extent([-180, 180, -90, -50], ccrs.PlateCarree())
+    ax1.set_boundary(circle, transform=ax1.transAxes)
+
+    gl1 = ax1.gridlines(crs=ccrs.PlateCarree(), draw_labels=False, linewidth=1, color='black', alpha=.8, linestyle='--')
+    gl1.xlocator = mticker.FixedLocator([-180, -135, -90, -45, 0, 45, 90, 135, 180])
+    gl1.ylocator = mticker.FixedLocator([-80, -70, -60, -50])
+
+    im1 = ax1.pcolormesh(lons, lats, 3600*24*365*melt_rate_field,
+                         transform=vector_crs, cmap=cmocean.cm.balance, vmin=-1, vmax=1)
+
+    ax1.contour(lons, lats, np.ma.array(climo_alpha_field, mask=np.isnan(climo_alpha_field)),
+                levels=[0.15], colors='black', linewidths=2, transform=vector_crs)
+    ax1.contour(lons, lats, np.ma.array(climo_tau_x_field, mask=np.isnan(climo_alpha_field)), levels=[0],
+                colors='green', linewidths=2, transform=vector_crs)
+
+    ax1.text(0.50, 1.05, r'$M-F v1', fontsize=14, va='bottom', ha='center', rotation='horizontal',
+             rotation_mode='anchor', transform=ax1.transAxes)
+
+    ax1.text(0.49, 1.01, '0°', transform=ax1.transAxes)
+    ax1.text(1.0105, 0.49, '90°E', transform=ax1.transAxes)
+    ax1.text(0.47, -0.03, '180°', transform=ax1.transAxes)
+    ax1.text(-0.09, 0.49, '90°W', transform=ax1.transAxes)
+    ax1.text(0.855, 0.895, '45°E', rotation=45, transform=ax1.transAxes)
+    ax1.text(0.85, 0.125, '135°E', rotation=-45, transform=ax1.transAxes)
+    ax1.text(0.07, 0.90, '45°W', rotation=-45, transform=ax1.transAxes)
+    ax1.text(0.06, 0.13, '135°W', rotation=45, transform=ax1.transAxes)
+
+    clb = fig.colorbar(im1, ax=ax1, extend='both', fraction=0.046, pad=0.1)
+    clb.ax.set_title(r'm/year')
+
+    ax2 = plt.subplot(122, projection=crs_sps)
+
+    ax2.add_feature(land_50m)
+    ax2.add_feature(ice_50m)
+    ax2.set_extent([-180, 180, -90, -50], ccrs.PlateCarree())
+    ax2.set_boundary(circle, transform=ax2.transAxes)
+
+    gl2 = ax2.gridlines(crs=ccrs.PlateCarree(), draw_labels=False, linewidth=1, color='black', alpha=.8, linestyle='--')
+    gl2.xlocator = mticker.FixedLocator([-180, -135, -90, -45, 0, 45, 90, 135, 180])
+    gl2.ylocator = mticker.FixedLocator([-80, -70, -60, -50])
+
+    im2 = ax2.pcolormesh(lons, lats,
+                         3600*24*365*np.add(np.divide(np.multiply(U_Ekman_field, dSdx_field), salinity_field),
+                                            np.divide(np.multiply(V_Ekman_field, dSdy_field), salinity_field)),
+                         transform=vector_crs, cmap=cmocean.cm.balance, vmin=-1, vmax=1)
+
+    ax2.contour(lons, lats, np.ma.array(climo_alpha_field, mask=np.isnan(climo_alpha_field)),
+                levels=[0.15], colors='black', linewidths=2, transform=vector_crs)
+    ax2.contour(lons, lats, np.ma.array(climo_tau_x_field, mask=np.isnan(climo_alpha_field)), levels=[0],
+                colors='green', linewidths=2, transform=vector_crs)
+
+    ax2.text(0.50, 1.05, r'M-F v2', fontsize=14, va='bottom', ha='center', rotation='horizontal',
+             rotation_mode='anchor', transform=ax2.transAxes)
+
+    ax2.text(0.49, 1.01, '0°', transform=ax2.transAxes)
+    ax2.text(1.0105, 0.49, '90°E', transform=ax2.transAxes)
+    ax2.text(0.47, -0.03, '180°', transform=ax2.transAxes)
+    ax2.text(-0.09, 0.49, '90°W', transform=ax2.transAxes)
+    ax2.text(0.855, 0.895, '45°E', rotation=45, transform=ax2.transAxes)
+    ax2.text(0.85, 0.125, '135°E', rotation=-45, transform=ax2.transAxes)
+    ax2.text(0.07, 0.90, '45°W', rotation=-45, transform=ax2.transAxes)
+    ax2.text(0.06, 0.13, '135°W', rotation=45, transform=ax2.transAxes)
+
+    clb = fig.colorbar(im2, ax=ax2, extend='both', fraction=0.046, pad=0.1)
+    clb.ax.set_title(r'm/year')
+
+    png_filepath = os.path.join(figure_dir_path, 'M-F.png')
+
+    tau_dir = os.path.dirname(png_filepath)
+    if not os.path.exists(tau_dir):
+        logger.info('Creating directory: {:s}'.format(tau_dir))
+        os.makedirs(tau_dir)
+
+    logger.info('Saving diagnostic figure: {:s}'.format(png_filepath))
+    plt.savefig(png_filepath, dpi=300, format='png', transparent=False, bbox_inches='tight')
+    plt.close()
+
+    """ Calculate streamwise coordinates """
     contour_coordinate = np.empty((len(lats), len(lons)))
     contour_coordinate[:] = np.nan
 
