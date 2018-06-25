cdcf76420fb90ee508319d9821f24e8eb5c4d9c5
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2018-06-25 11:06:02 -0400

    Looking for the ice-ocean governor in the Southern Ocean.

--- make_figures.py
+++ make_figures.py
@@ -2716,6 +2716,155 @@ def make_melt_rate_plots():
     plt.savefig(png_filepath, dpi=300, format='png', transparent=False)
 
 
+def look_for_ice_ocean_governor():
+    from utils import get_netCDF_filepath, get_field_from_netcdf
+    from constants import figure_dir_path
+
+    import constants
+    constants.output_dir_path = 'D:\\output\\'
+
+    climo_filepath = get_netCDF_filepath(field_type='climo', year_start=2005, year_end=2015)
+    # climo_filepath = get_netCDF_filepath(field_type='seasonal_climo', season_str='JAS',
+    #                                      year_start=2005, year_end=2015)
+    # climo_filepath = get_netCDF_filepath(field_type='seasonal_climo', season_str='JFM',
+    #                                      year_start=2005, year_end=2015)
+
+    lons, lats, climo_tau_x_field_geo = get_field_from_netcdf(climo_filepath, 'tau_x')
+    climo_w_Ekman_field_geo = get_field_from_netcdf(climo_filepath, 'Ekman_w')[2]
+
+    constants.output_dir_path = 'C:\\Users\\Ali\\Downloads\\output\\'
+
+    climo_filepath = get_netCDF_filepath(field_type='climo', year_start=2005, year_end=2015)
+    # climo_filepath = get_netCDF_filepath(field_type='seasonal_climo', season_str='JAS',
+    #                                      year_start=2005, year_end=2015)
+    # climo_filepath = get_netCDF_filepath(field_type='seasonal_climo', season_str='JFM',
+    #                                      year_start=2005, year_end=2015)
+
+    feb_climo_filepath = get_netCDF_filepath(field_type='monthly_climo', date=datetime.date(2005, 2, 1),
+                                             year_start=2005, year_end=2015)
+    sep_climo_filepath = get_netCDF_filepath(field_type='monthly_climo', date=datetime.date(2005, 9, 1),
+                                             year_start=2005, year_end=2015)
+
+    lons, lats, climo_tau_x_field = get_field_from_netcdf(climo_filepath, 'tau_x')
+    climo_alpha_field = get_field_from_netcdf(climo_filepath, 'alpha')[2]
+    climo_w_Ekman_field = get_field_from_netcdf(climo_filepath, 'Ekman_w')[2]
+
+    feb_climo_alpha_field = get_field_from_netcdf(feb_climo_filepath, 'alpha')[2]
+    sep_climo_alpha_field = get_field_from_netcdf(sep_climo_filepath, 'alpha')[2]
+
+    # Add land to the plot with a 1:50,000,000 scale. Line width is set to 0 so that the edges aren't poofed up in
+    # the smaller plots.
+    land_50m = cartopy.feature.NaturalEarthFeature('physical', 'land', '50m', edgecolor='face', facecolor='dimgray',
+                                                   linewidth=0)
+    ice_50m = cartopy.feature.NaturalEarthFeature('physical', 'antarctic_ice_shelves_polys', '50m', edgecolor='face',
+                                                  facecolor='darkgray', linewidth=0)
+    vector_crs = ccrs.PlateCarree()
+
+    fig = plt.figure(figsize=(16, 9))
+    gs = GridSpec(1, 2)
+    matplotlib.rcParams.update({'font.size': 10})
+
+    # Compute a circle in axes coordinates, which we can use as a boundary
+    # for the map. We can pan/zoom as much as we like - the boundary will be
+    # permanently circular.
+    import matplotlib.path as mpath
+    theta = np.linspace(0, 2*np.pi, 100)
+    center, radius = [0.5, 0.5], 0.5
+    verts = np.vstack([np.sin(theta), np.cos(theta)]).T
+    circle = mpath.Path(verts * radius + center)
+
+    """ Plot Ekman pumping (without u_geo) """
+    ax1 = plt.subplot(121, projection=ccrs.SouthPolarStereo())
+
+    ax1.add_feature(land_50m)
+    ax1.add_feature(ice_50m)
+    ax1.set_extent([-180, 180, -90, -50], ccrs.PlateCarree())
+    ax1.set_boundary(circle, transform=ax1.transAxes)
+
+    gl1 = ax1.gridlines(crs=ccrs.PlateCarree(), draw_labels=False, linewidth=1, color='black', alpha=.8, linestyle='--')
+    gl1.xlocator = mticker.FixedLocator([-180, -135, -90, -45, 0, 45, 90, 135, 180])
+    gl1.ylocator = mticker.FixedLocator([-80, -70, -60, -50])
+
+    im1 = ax1.pcolormesh(lons, lats, climo_w_Ekman_field * 365*24*3600, transform=vector_crs, cmap=cmocean.cm.balance,
+                         vmin=-100, vmax=100)
+
+    ax1.contour(lons, lats, np.ma.array(climo_tau_x_field, mask=np.isnan(climo_alpha_field)), levels=[0],
+                colors='green', linewidths=2, transform=vector_crs)
+    ax1.contour(lons, lats, np.ma.array(feb_climo_alpha_field, mask=np.isnan(feb_climo_alpha_field)),
+                levels=[0.15], colors='black', linewidths=2, transform=vector_crs)
+    ax1.contour(lons, lats, np.ma.array(sep_climo_alpha_field, mask=np.isnan(sep_climo_alpha_field)),
+                levels=[0.15], colors='black', linewidths=2, transform=vector_crs)
+
+    ax1.text(0.49,  1.01,  '0°',   transform=ax1.transAxes)
+    ax1.text(1.01,  0.49,  '90°E', transform=ax1.transAxes)
+    ax1.text(0.47,  -0.03, '180°', transform=ax1.transAxes)
+    ax1.text(-0.09, 0.49,  '90°W', transform=ax1.transAxes)
+    ax1.text(0.855, 0.895, '45°E',  rotation=45,  transform=ax1.transAxes)
+    ax1.text(0.85,  0.125, '135°E', rotation=-45, transform=ax1.transAxes)
+    ax1.text(0.07,  0.90,  '45°W',  rotation=-45, transform=ax1.transAxes)
+    ax1.text(0.06,  0.13,  '135°W', rotation=45,  transform=ax1.transAxes)
+
+    ax1.text(0.50, 1.05, 'Ekman pumping (no u_geo)', fontsize=14, ha='center', transform=ax1.transAxes)
+
+    clb = fig.colorbar(im1, ax=ax1, extend='both', fraction=0.046, pad=0.1)
+    clb.ax.set_title('m/year')
+
+    zero_stress_line_patch = mpatches.Patch(color='green', label='zero zonal stress line')
+    ice_edge_patch = mpatches.Patch(color='black', label=r'15% ice edge')
+    plt.legend(handles=[zero_stress_line_patch, ice_edge_patch], loc='lower center',
+               bbox_to_anchor=(0, -0.1, 1, -0.1), ncol=1, mode='expand', borderaxespad=0, framealpha=0)
+
+    # plt.suptitle(r'Figure 6: Surface stress curl and Ekman pumping observations, winter (JAS) mean', fontsize=16)
+
+    """ Plot Ekman pumping (including u_geo) """
+    ax2 = plt.subplot(122, projection=ccrs.SouthPolarStereo())
+
+    ax2.add_feature(land_50m)
+    ax2.add_feature(ice_50m)
+    ax2.set_extent([-180, 180, -90, -50], ccrs.PlateCarree())
+    ax2.set_boundary(circle, transform=ax2.transAxes)
+
+    gl2 = ax2.gridlines(crs=ccrs.PlateCarree(), draw_labels=False, linewidth=1, color='black', alpha=.8, linestyle='--')
+    gl2.xlocator = mticker.FixedLocator([-180, -135, -90, -45, 0, 45, 90, 135, 180])
+    gl2.ylocator = mticker.FixedLocator([-80, -70, -60, -50])
+
+    climo_w_Ekman_field_geo, lons_cyclic = cartopy.util.add_cyclic_point(climo_w_Ekman_field_geo, coord=lons)
+
+    im2 = ax2.pcolormesh(lons_cyclic, lats, climo_w_Ekman_field_geo * 365*24*3600, transform=vector_crs,
+                         cmap=cmocean.cm.balance, vmin=-100, vmax=100)
+
+    ax2.contour(lons, lats, np.ma.array(climo_tau_x_field_geo, mask=np.isnan(climo_alpha_field)), levels=[0],
+                colors='green', linewidths=2, transform=vector_crs)
+    ax2.contour(lons, lats, np.ma.array(feb_climo_alpha_field, mask=np.isnan(feb_climo_alpha_field)),
+                levels=[0.15], colors='black', linewidths=2, transform=vector_crs)
+    ax2.contour(lons, lats, np.ma.array(sep_climo_alpha_field, mask=np.isnan(sep_climo_alpha_field)),
+                levels=[0.15], colors='black', linewidths=2, transform=vector_crs)
+
+    ax2.text(0.49,  1.01,  '0°',   transform=ax2.transAxes)
+    ax2.text(1.01,  0.49,  '90°E', transform=ax2.transAxes)
+    ax2.text(0.47,  -0.03, '180°', transform=ax2.transAxes)
+    ax2.text(-0.09, 0.49,  '90°W', transform=ax2.transAxes)
+    ax2.text(0.855, 0.895, '45°E',  rotation=45,  transform=ax2.transAxes)
+    ax2.text(0.85,  0.125, '135°E', rotation=-45, transform=ax2.transAxes)
+    ax2.text(0.07,  0.90,  '45°W',  rotation=-45, transform=ax2.transAxes)
+    ax2.text(0.06,  0.13,  '135°W', rotation=45,  transform=ax2.transAxes)
+
+    ax2.text(0.50, 1.05, 'Ekman pumping (including u_geo)', fontsize=14, ha='center', transform=ax2.transAxes)
+
+    clb = fig.colorbar(im2, ax=ax2, extend='both', fraction=0.046, pad=0.1)
+    clb.ax.set_title(r'm/year')
+
+    png_filepath = os.path.join(figure_dir_path, 'ice_ocean_governor.png')
+
+    tau_dir = os.path.dirname(png_filepath)
+    if not os.path.exists(tau_dir):
+        logger.info('Creating directory: {:s}'.format(tau_dir))
+        os.makedirs(tau_dir)
+
+    logger.info('Saving diagnostic figure: {:s}'.format(png_filepath))
+    plt.savefig(png_filepath, dpi=300, format='png', transparent=False)  # , bbox_inches='tight')
+
+
 if __name__ == '__main__':
     # make_five_box_climo_fig('tau_x')
     # make_five_box_climo_fig('tau_y')
