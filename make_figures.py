9a3cf5780dd7a91d65dbf0e2bc9bd44579e258f4
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2018-06-25 10:58:32 -0400

    Figure 1 with key observations.

--- make_figures.py
+++ make_figures.py
@@ -1687,6 +1687,137 @@ def make_curl_climo_fig():
     plt.savefig(png_filepath, dpi=300, format='png', transparent=False)  # , bbox_inches='tight')
 
 
+def make_figure1():
+    from utils import get_netCDF_filepath, get_field_from_netcdf
+    from constants import figure_dir_path
+
+    # climo_filepath = get_netCDF_filepath(field_type='climo', year_start=2005, year_end=2015)
+    climo_filepath = get_netCDF_filepath(field_type='seasonal_climo', season_str='JAS',
+                                         year_start=2005, year_end=2015)
+    # climo_filepath = get_netCDF_filepath(field_type='seasonal_climo', season_str='JFM',
+    #                                      year_start=2005, year_end=2015)
+
+    feb_climo_filepath = get_netCDF_filepath(field_type='monthly_climo', date=datetime.date(2005, 2, 1),
+                                             year_start=2005, year_end=2015)
+    sep_climo_filepath = get_netCDF_filepath(field_type='monthly_climo', date=datetime.date(2005, 9, 1),
+                                             year_start=2005, year_end=2015)
+
+    JAS_climo_filepath = get_netCDF_filepath(field_type='seasonal_climo', season_str='JAS',
+                                             year_start=2005, year_end=2015)
+    JFM_climo_filepath = get_netCDF_filepath(field_type='seasonal_climo', season_str='JFM',
+                                             year_start=2005, year_end=2015)
+
+    lons, lats, climo_tau_x_field = get_field_from_netcdf(climo_filepath, 'tau_x')
+    climo_tau_y_field = get_field_from_netcdf(climo_filepath, 'tau_y')[2]
+    climo_alpha_field = get_field_from_netcdf(climo_filepath, 'alpha')[2]
+    climo_u_wind_field = get_field_from_netcdf(climo_filepath, 'wind_u')[2]
+    climo_v_wind_field = get_field_from_netcdf(climo_filepath, 'wind_v')[2]
+    climo_u_ice_field = get_field_from_netcdf(climo_filepath, 'ice_u')[2]
+    climo_v_ice_field = get_field_from_netcdf(climo_filepath, 'ice_v')[2]
+
+    feb_climo_alpha_field = get_field_from_netcdf(feb_climo_filepath, 'alpha')[2]
+    sep_climo_alpha_field = get_field_from_netcdf(sep_climo_filepath, 'alpha')[2]
+
+    JAS_climo_alpha_field = get_field_from_netcdf(JAS_climo_filepath, 'alpha')[2]
+    JFM_climo_alpha_field = get_field_from_netcdf(JFM_climo_filepath, 'alpha')[2]
+
+    # Add land to the plot with a 1:50,000,000 scale. Line width is set to 0 so that the edges aren't poofed up in
+    # the smaller plots.
+    land_50m = cartopy.feature.NaturalEarthFeature('physical', 'land', '50m', edgecolor='face', facecolor='dimgray',
+                                                   linewidth=0)
+    ice_50m = cartopy.feature.NaturalEarthFeature('physical', 'antarctic_ice_shelves_polys', '50m', edgecolor='face',
+                                                  facecolor='darkgray', linewidth=0)
+    vector_crs = ccrs.PlateCarree()
+
+    fig = plt.figure(figsize=(16, 9))
+    matplotlib.rcParams.update({'font.size': 10})
+
+    crs_sps = ccrs.SouthPolarStereo()
+    crs_sps._threshold = 1000.0  # This solves https://github.com/SciTools/cartopy/issues/363
+    ax = plt.subplot(111, projection=crs_sps)
+
+    # Compute a circle in axes coordinates, which we can use as a boundary
+    # for the map. We can pan/zoom as much as we like - the boundary will be
+    # permanently circular.
+    import matplotlib.path as mpath
+    theta = np.linspace(0, 2*np.pi, 100)
+    center, radius = [0.5, 0.5], 0.5
+    verts = np.vstack([np.sin(theta), np.cos(theta)]).T
+    circle = mpath.Path(verts * radius + center)
+    ax.set_boundary(circle, transform=ax.transAxes)
+
+    ax.add_feature(land_50m)
+    ax.add_feature(ice_50m)
+    ax.set_extent([-180, 180, -90, -50], ccrs.PlateCarree())
+
+    im = ax.pcolormesh(lons, lats, climo_alpha_field, transform=vector_crs, cmap='Blues', vmin=-0, vmax=1)
+
+    gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=False, linewidth=1, color='black', alpha=.8, linestyle='--')
+    gl.xlocator = mticker.FixedLocator([-180, -135, -90, -45, 0, 45, 90, 135, 180])
+    gl.ylocator = mticker.FixedLocator([-80, -70, -60, -50])
+
+    Q1 = ax.quiver(lons[::10], lats[::10],
+                   np.ma.array(climo_u_wind_field, mask=(sep_climo_alpha_field > 0.15))[::10, ::10],
+                   np.ma.array(climo_v_wind_field, mask=(sep_climo_alpha_field > 0.15))[::10, ::10],
+                   pivot='middle', color='green', transform=vector_crs, units='width', width=0.002, scale=250)
+    Q2 = ax.quiver(lons[::10], lats[::10],
+                   np.ma.array(climo_u_wind_field, mask=(sep_climo_alpha_field < 0.15))[::10, ::10],
+                   np.ma.array(climo_v_wind_field, mask=(sep_climo_alpha_field < 0.15))[::10, ::10],
+                   pivot='middle', color='yellow', transform=vector_crs, units='width', width=0.002, scale=150)
+
+    Q3 = ax.quiver(lons[::10], lats[::10],
+                   np.ma.array(climo_u_ice_field, mask=(sep_climo_alpha_field < 0.15))[::10, ::10],
+                   np.ma.array(climo_v_ice_field, mask=(sep_climo_alpha_field < 0.15))[::10, ::10],
+                   pivot='middle', color='#E8000B', transform=vector_crs, units='width', width=0.002, scale=2)
+
+    plt.quiverkey(Q2, 0.70, 0.86, 10, r'10 m/s (wind, inside ice zone)', labelpos='E', coordinates='figure',
+                  fontproperties={'size': 11}, transform=ax.transAxes)
+    plt.quiverkey(Q1, 0.70, 0.83, 10, r'10 m/s (wind, outside ice zone)', labelpos='E', coordinates='figure',
+                  fontproperties={'size': 11}, transform=ax.transAxes)
+    plt.quiverkey(Q3, 0.73, 0.80, 0.05, r'5 cm/s (ice drift)', labelpos='E', coordinates='figure',
+                  fontproperties={'size': 11}, transform=ax.transAxes)
+
+    # ax.contour(lons, lats, np.ma.array(feb_climo_alpha_field, mask=np.isnan(feb_climo_alpha_field)),
+    #            levels=[0.15], colors='black', linewidths=2, transform=vector_crs)
+    # ax.contour(lons, lats, np.ma.array(sep_climo_alpha_field, mask=np.isnan(sep_climo_alpha_field)),
+    #            levels=[0.15], colors='black', linewidths=2, transform=vector_crs)
+    ax.contour(lons, lats, np.ma.array(JAS_climo_alpha_field, mask=np.isnan(JAS_climo_alpha_field)),
+               levels=[0.15], colors='black', linewidths=2, transform=vector_crs)
+    ax.contour(lons, lats, np.ma.array(JFM_climo_alpha_field, mask=np.isnan(JFM_climo_alpha_field)),
+               levels=[0.15], colors='black', linewidths=2, transform=vector_crs)
+
+    ax.text(0.49,  1.01,  '0°',   transform=ax.transAxes)
+    ax.text(1.01,  0.49,  '90°E', transform=ax.transAxes)
+    ax.text(0.47,  -0.03, '180°', transform=ax.transAxes)
+    ax.text(-0.09, 0.49,  '90°W', transform=ax.transAxes)
+    ax.text(0.855, 0.895, '45°E',  rotation=45,  transform=ax.transAxes)
+    ax.text(0.85,  0.125, '135°E', rotation=-45, transform=ax.transAxes)
+    ax.text(0.07,  0.90,  '45°W',  rotation=-45, transform=ax.transAxes)
+    ax.text(0.06,  0.13,  '135°W', rotation=45,  transform=ax.transAxes)
+
+    clb = fig.colorbar(im, ax=ax, fraction=0.046, pad=0.1)
+    clb.ax.set_title(r'sea ice fraction $\alpha$')
+
+    # plt.gcf().text(0.5, 0.95, 'Figure 1(a): key observations, annual mean', fontsize=14)
+    plt.gcf().text(0.5, 0.95, 'Figure 1(a): key observations, winter (JAS) mean', fontsize=14)
+    # plt.gcf().text(0.5, 0.95, 'Figure 1(a): key observations, summer (JFM) mean', fontsize=14)
+
+    # zero_stress_line_patch = mpatches.Patch(color='green', label='zero zonal stress line')
+    # gamma_contour_patch = mpatches.Patch(color='black', label=r'$\gamma$ = 27.6 kg/m$^3$ contour')
+    # plt.legend(handles=[zero_stress_line_patch, gamma_contour_patch], loc='lower center',
+    #            bbox_to_anchor=(0, -0.05, 1, -0.05), ncol=1, mode='expand', borderaxespad=0, framealpha=0)
+
+    png_filepath = os.path.join(figure_dir_path, 'figure1.png')
+
+    tau_dir = os.path.dirname(png_filepath)
+    if not os.path.exists(tau_dir):
+        logger.info('Creating directory: {:s}'.format(tau_dir))
+        os.makedirs(tau_dir)
+
+    logger.info('Saving diagnostic figure: {:s}'.format(png_filepath))
+    plt.savefig(png_filepath, dpi=300, format='png', transparent=False, bbox_inches='tight')
+
+
 if __name__ == '__main__':
     # make_five_box_climo_fig('tau_x')
     # make_five_box_climo_fig('tau_y')
