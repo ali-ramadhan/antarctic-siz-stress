e24633869ae0301a7b79fcdfc8d88f95ef4fb729
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2018-10-29 09:06:17 -0400

    Repetitive code generation for Weddell script.

--- generate_repetitive_code.py
+++ generate_repetitive_code.py
@@ -17,12 +17,26 @@ varz2 = ['alpha', 'u_ice', 'v_ice', 'h_ice', 'zonal_div', 'merid_div', 'div',
          'hu_dadx', 'au_dhdx', 'ah_dudx', 'hv_dady', 'av_dhdy', 'ah_dvdy', 'div2',
          'salinity']
 
-for var in varz2:
-    print('{:s}_avg_field = {:s}_avg_field + np.nan_to_num({:s}_daily_field)'.format(var, var, var))
-    print('{:s}_daily_field[~np.isnan({:s}_daily_field)] = 1'.format(var, var))
-    print('{:s}_daily_field[np.isnan({:s}_daily_field)] = 0'.format(var, var))
-    print('{:s}_day_field = {:s}_day_field + {:s}_daily_field'.format(var, var, var))
-    print('')
+varz2prime = ['alpha', 'tau_io_x_nogeo', 'tau_io_y_nogeo', 'tau_io_x_geo', 'tau_io_y_geo', 'tau_ig_x', 'tau_ig_y',
+              'tau_ao_x', 'tau_ao_y', 'w_Ek_nogeo', 'w_Ek_geo', 'w_a', 'w_i', 'w_i0', 'w_ig', 'w_A', 'gamma']
 
-for var in varz2:
+# for var in varz2prime:
+#     print('{:s}_avg_field = {:s}_avg_field + np.nan_to_num({:s}_daily_field)'.format(var, var, var))
+#     print('{:s}_daily_field[~np.isnan({:s}_daily_field)] = 1'.format(var, var))
+#     print('{:s}_daily_field[np.isnan({:s}_daily_field)] = 0'.format(var, var))
+#     print('{:s}_day_field = {:s}_day_field + {:s}_daily_field'.format(var, var, var))
+#     print('')
+#
+for var in varz2prime:
     print('{:s}_avg_field = np.divide({:s}_avg_field, {:s}_day_field)'.format(var, var, var))
+
+# varz3 = ['alpha', 'tau_ao_x', 'tau_ao_y', 'tau_io_x_nogeo', 'tau_io_y_nogeo', 'tau_io_x_geo', 'tau_io_y_geo',
+#          'tau_ig_x', 'tau_ig_y']
+#
+# for var in varz3:
+#     print('{:s}_ij = {:s}_daily_field[i][j]'.format(var, var))
+#     print('{:s}_i_jp1 = {:s}_daily_field[i][jp1]'.format(var, var))
+#     print('{:s}_i_jm1 = {:s}_daily_field[i][jm1]'.format(var, var))
+#     print('{:s}_ip1_j = {:s}_daily_field[i+1][j]'.format(var, var))
+#     print('{:s}_im1_j = {:s}_daily_field[i-1][j]'.format(var, var))
+#     print("")
